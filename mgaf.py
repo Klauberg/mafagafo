c0d0c492a10fde63c083dec197b26c648dcbfd4c
Guilherme Sehn <ghsehn@gmail.com>
2015-09-25 21:21:59 -0300

    tabs convertidos para espaços

--- mgaf.py
+++ mgaf.py
@@ -5,4 +5,3 @@ gramatica = Gramatica(['S'], ['a', 'b'], {'S': ['a', 'b'], 'SS': ['b']})
 gerador_formalismo_gramatica = GeradorFormalismoGramatica(gramatica)
 
 print gerador_formalismo_gramatica.gerar()
-
