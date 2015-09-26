deb1b24482958117d0385b0e34a7886d2f726d86
Guilherme Sehn <ghsehn@gmail.com>
2015-09-25 21:23:01 -0300

    todas as strings com aspas simples

--- gerador_formalismo_gramatica.py
+++ gerador_formalismo_gramatica.py
@@ -15,15 +15,15 @@ class GeradorFormalismoGramatica:
         str = '{'
 
         for nao_terminais, terminais in self.gramatica.conjunto_producoes.iteritems():
-            str += "\n  " + nao_terminais + ' -> '
+            str += '\n  ' + nao_terminais + ' -> '
             str += '|'.join(terminais)
 
-        str += "\n}"
+        str += '\n}'
         return str
 
     def gerar(self):
         str = 'G = (%s, %s, P, S)' % (self.simbolos_nao_terminais, self.simbolos_terminais)
-        str += "\nP = %s" % self.conjunto_producoes
+        str += '\nP = %s' % self.conjunto_producoes
         return str
 
     def __gerar_simbolos(self, simbolos):
