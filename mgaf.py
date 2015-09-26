7dbba8a26045f30bfbf5be0599771dddd0d8b83d
Guilherme Sehn <ghsehn@gmail.com>
2015-09-26 10:46:17 -0300

    classes de gramática movidas para uma pasta a parte

--- mgaf.py
+++ mgaf.py
@@ -1,7 +1,13 @@
-from gramatica import Gramatica
-from gerador_formalismo_gramatica import GeradorFormalismoGramatica
+from gramatica.gramatica import Gramatica
+from gramatica.gerador_formalismo_gramatica import GeradorFormalismoGramatica
+from gramatica.identificador_tipo_gramatica import IdentificadorTipoGramatica
 
 gramatica = Gramatica(['S'], ['a', 'b'], {'S': ['a', 'b'], 'SS': ['b']})
-gerador_formalismo_gramatica = GeradorFormalismoGramatica(gramatica)
 
+# gera formalismo
+gerador_formalismo_gramatica = GeradorFormalismoGramatica(gramatica)
 print gerador_formalismo_gramatica.gerar()
+
+# identifica tipo
+identificador_tipo_gramatica = IdentificadorTipoGramatica(gramatica)
+print identificador_tipo_gramatica.identificar()
