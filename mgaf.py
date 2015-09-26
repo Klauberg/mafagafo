48a75ac4493a9d8cb0fea16f734645a0f556a54e
Guilherme Sehn <ghsehn@gmail.com>
2015-09-26 10:49:16 -0300

    Removidas chamadas à classe de identificador

--- mgaf.py
+++ mgaf.py
@@ -1,13 +1,8 @@
 from gramatica.gramatica import Gramatica
 from gramatica.gerador_formalismo_gramatica import GeradorFormalismoGramatica
-from gramatica.identificador_tipo_gramatica import IdentificadorTipoGramatica
 
 gramatica = Gramatica(['S'], ['a', 'b'], {'S': ['a', 'b'], 'SS': ['b']})
 
 # gera formalismo
 gerador_formalismo_gramatica = GeradorFormalismoGramatica(gramatica)
 print gerador_formalismo_gramatica.gerar()
-
-# identifica tipo
-identificador_tipo_gramatica = IdentificadorTipoGramatica(gramatica)
-print identificador_tipo_gramatica.identificar()
