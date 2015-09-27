faa8d2d1560399ee71cfdd742a635f3d8b2252d7
Guilherme Sehn <ghsehn@gmail.com>
2015-09-27 14:07:57 -0300

    refactoring no gerador de sentenças

--- gramatica/gerador_sentencas.py
+++ gramatica/gerador_sentencas.py
@@ -16,21 +16,28 @@ class GeradorSentencas:
         historico = [sentenca]  
         i = 0
 
-        while re.search('[A-Z]', sentenca):
+        while self.contem_nt(sentenca):
             i += 1
             if i > self.MAXIMO_ITERACOES:
                 overflow = True
                 break
-
-            for esquerda, direita in self.conjunto_producoes.iteritems():
-                if esquerda in sentenca:
-                    indice = random.randint(0, len(direita) - 1)
-                    item_direita = direita[indice]
-                    sentenca = sentenca.replace(esquerda, item_direita)
-                    historico.append(sentenca)
-                    break
+            sentenca = self.gerar_derivacao(sentenca)
+            historico.append(sentenca)
 
         return (sentenca, historico, overflow)
+
+    def gerar_derivacao(self, sentenca):
+        for esquerda, direita in self.conjunto_producoes.iteritems():
+            if esquerda in sentenca:
+                indice = random.randint(0, len(direita) - 1)
+                item_direita = direita[indice]
+                sentenca = sentenca.replace(esquerda, item_direita)
+                break
+        return sentenca
+
+    def contem_nt(self, sentenca):
+        """Verifica se sentença contém símbolo não-terminal"""
+        return bool(re.search('[A-Z]', sentenca))
 
     def ordenar_conjunto_producoes(self, conjunto_producoes):
         """
