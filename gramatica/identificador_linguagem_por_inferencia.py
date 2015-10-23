83d1d9598c1a69d4061bb4050874ae57ad3afa94
Guilherme Sehn <ghsehn@gmail.com>
2015-10-23 18:00:36 -0200

    adicionado sys.maxsize, métodos renomeados

--- gramatica/identificador_linguagem_por_inferencia.py
+++ gramatica/identificador_linguagem_por_inferencia.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import re
+import sys
 from constants import *
 
 class IdentificadorLinguagemPorInferencia:
@@ -7,21 +8,17 @@ class IdentificadorLinguagemPorInferencia:
         self.sentencas = sentencas
 
     def identificar(self):
-        # Implementar lógica aqui
-        # Dá para acessar as sentenças geradas aqui por self.sentencas
-        #print '\n'.join(self.sentencas)
-
-        lista = {} #lista que representa cada sentenca
+        lista = {} # lista que representa cada sentenca
         for s in self.sentencas:
             for c in s:
                 lista.update({c:'-'})
         for l in lista:
-            vmin = self.verificarMinimo(l)
+            vmin = self.verificar_minimo(l)
             lista.update({l:'>='+str(vmin)})
         for l in lista:
             for l2 in lista:
-                if l!=l2 and self.verificarMultiplo(l, l2)!=0:
-                    lista[l] = '='+str(self.verificarMultiplo(l, l2))+l2 
+                if l!=l2 and self.verificar_multiplo(l, l2)!=0:
+                    lista[l] = '='+str(self.verificar_multiplo(l, l2))+l2 
 
         return self.formalizar(lista)
 
@@ -39,14 +36,14 @@ class IdentificadorLinguagemPorInferencia:
         valor += '|'
         for v in variaveis:
             if not '>' in v:
-                last = v[-1] #pega o ultimo caractere v = '=2a' -> last = 'a'
+                last = v[-1] # pega o ultimo caractere v = '=2a' -> last = 'a'
                 valor += variaveis[v]+v.replace(last, variaveis[lista[last]])+', '
             else:
                 valor += variaveis[v]+str(v)+', '
         valor += ')'
         return valor
 
-    def verificarMultiplo(self, t1, t2):
+    def verificar_multiplo(self, t1, t2):
         div = 0
         for s in self.sentencas:
             if t1 in s and t2 in s:
@@ -59,8 +56,8 @@ class IdentificadorLinguagemPorInferencia:
         if div == 1: return 0
         return div
 
-    def verificarMinimo(self, t):
-        minimo = 999999 # sys.maxsize não ta funcionando
+    def verificar_minimo(self, t):
+        minimo = sys.maxsize
         for s in self.sentencas:
             if s.count(t) < minimo:
                 minimo = s.count(t)
