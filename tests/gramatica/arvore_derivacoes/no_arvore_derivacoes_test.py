0e9a639fbb299dea0443e729ee293733fe5835e0
Guilherme Sehn <ghsehn@gmail.com>
2015-10-24 12:02:01 -0200

    Cobertura de testes aumentada para NoArvoreDerivacoes

--- tests/gramatica/arvore_derivacoes/no_arvore_derivacoes_test.py
+++ tests/gramatica/arvore_derivacoes/no_arvore_derivacoes_test.py
@@ -19,3 +19,34 @@ class NoArvoreDerivacoesTest(unittest.TestCase):
 
         for no in no2.nos:
             self.assertEqual(no.nao_terminais_derivados(), set(['S', 'A', 'B']))
+
+    def test_contem_recursao(self):
+        no1 = NoArvoreDerivacoes('S')
+
+        no2 = NoArvoreDerivacoes('aBAa', no1)
+        no1.nos.append(no2)
+        self.assertFalse(no2.contem_recursao())
+
+        no3 = NoArvoreDerivacoes('accAa', no2)
+        no2.nos.append(no3)
+        self.assertTrue(no3.contem_recursao())
+
+    def test_quantidade_derivacoes(self):
+        no1 = NoArvoreDerivacoes('S')
+        self.assertEqual(no1.quantidade_derivacoes(), 0)
+
+        no2 = NoArvoreDerivacoes('A', no1)
+        no1.nos.append(no2)
+        self.assertEqual(no2.quantidade_derivacoes(), 1)
+
+        no3 = NoArvoreDerivacoes('B', no2)
+        no2.nos.append(no3)
+        self.assertEqual(no3.quantidade_derivacoes(), 2)
+
+        no4 = NoArvoreDerivacoes('C', no3)
+        no3.nos.append(no4)
+        self.assertEqual(no4.quantidade_derivacoes(), 3)
+
+        no5 = NoArvoreDerivacoes('a', no4)
+        no4.nos.append(no5)
+        self.assertEqual(no5.quantidade_derivacoes(), 4)
