997cd8a43fc1ecb6d6e0ea812e921577d087ef21
Guilherme Sehn <ghsehn@gmail.com>
2015-10-24 12:44:37 -0200

    Validação de sentença vazia no lado esquerdo

--- interface/validacoes.py
+++ interface/validacoes.py
@@ -2,16 +2,13 @@
 import re
 from constants import *
 
-def validar_sentenca_vazia(simbolo):
-    if any(s == SIMBOLO_SENTENCA_VAZIA for s in simbolos):
-        return (False, 'Você não pode informar o símbolo %s aqui, pois ele é o caractere' \
-            'que representa a sentença vazia nesse programa.' % SIMBOLO_SENTENCA_VAZIA)
-
-    return (True,)
-
 def validar_lado_esquerdo(simbolos):
     if not re.search('[A-Z]', simbolos):
         return (False, 'O lado esquerdo deve conter ao menos um símbolo não-terminal')
+
+    if SIMBOLO_SENTENCA_VAZIA in simbolos:
+        return (False, 'O símbolo de sentença vazia (%s) não pode estar no lado esquerdo' \
+            ' do conjunto de produções.' % SIMBOLO_SENTENCA_VAZIA)
 
     return (True,)
 
