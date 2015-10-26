fa909ed96df7d604f3310f1d44d04434db858882
Guilherme Sehn <ghsehn@gmail.com>
2015-10-26 21:07:19 -0200

    Mais um pequeno teste na Gramatica

--- tests/gramatica/gramatica_test.py
+++ tests/gramatica/gramatica_test.py
@@ -6,13 +6,15 @@ from constants import *
 class GramaticaTest(unittest.TestCase):
     NAO_TERMINAIS = ['S', 'A']
     TERMINAIS = ['a', 'b']
+    SIMBOLO_INICIAL = 'S'
     CONJUNTO_PRODUCOES = {
         'S': ['aA', 'a'],
         'A': ['b', 'A']
     }
 
     def setUp(self):
-        self.gramatica = Gramatica(self.NAO_TERMINAIS, self.TERMINAIS, self.CONJUNTO_PRODUCOES, 'S')
+        self.gramatica = Gramatica(self.NAO_TERMINAIS, self.TERMINAIS,
+            self.CONJUNTO_PRODUCOES, self.SIMBOLO_INICIAL)
 
     def test_simbolos_nao_terminais(self):
         self.assertEqual(self.gramatica.simbolos_nao_terminais, self.NAO_TERMINAIS)
@@ -22,3 +24,6 @@ class GramaticaTest(unittest.TestCase):
 
     def test_simbolos_conjunto_producoes(self):
         self.assertEqual(self.gramatica.conjunto_producoes, self.CONJUNTO_PRODUCOES)
+
+    def test_simbolo_inicial(self):
+        self.assertEqual(self.gramatica.simbolo_inicial, self.SIMBOLO_INICIAL)
