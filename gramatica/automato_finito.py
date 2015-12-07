1628590be64bebfa9cfe12571a3054bfa5832ce7
Eduardo <eduardogc@mx2.unisc.br>
2015-12-07 10:30:49 -0200

    Reconhecendo um AF

--- gramatica/automato_finito.py
+++ gramatica/automato_finito.py
@@ -13,29 +13,35 @@ class AutomatoFinito:
 
     def reconhecer(self, atual, sentenca, automato):
         print atual+' '+sentenca;
-        if sentenca == '': return atual
-        if not atual in automato['regras']:
-            return ''
+        if sentenca == '': 
+            if atual in automato['fim']: 
+                return True, atual 
+            else:
+                return False, atual
         for x in automato['regras'][atual]:
             if sentenca[0] in automato['regras'][atual][x]:
-                acho = True
                 atual = x
                 sentenca = sentenca[1:];
-                if self.reconhecer(atual, sentenca, automato) != '': return atual 
+                saida = self.reconhecer(atual, sentenca, automato)
+                if saida[0]: return True, saida[1]
+        return False, atual
 
             
     def testar(self):
         #exemplo de estrutura de um automato
         estados = ['q0', 'q1', 'q2']
         simbolos = ['a', 'b', 'c']
-        regras = {'q0':{'q0':'b', 'q1':'a'}, 'q1':{'q2':'ab'}}
+        regras = {'q0':{'q0':'b', 'q1':'a', 'q2':''}, 'q1':{'q0': '', 'q1':'', 'q2':'ab'}, 'q2':{'q0':'', 'q1':'', 'q2':''}}
         inicio = 'q0'
         fim = ['q2']
 
         automato = {'estados':estados, 'simbolos':simbolos, 'regras':regras, 'inicio':inicio, 'fim':fim}
-        sentenca = 'bbaac'
+        sentenca = 'bbbbbbbaa'
 
         saida = self.reconhecer('q0', sentenca, automato)
-        print 'Saida: '+saida
+        if saida[0]:
+            print 'Reconheceu: '+saida[1]
+        else:
+            print 'Nao Reconheceu: '+saida[1]
 
 
